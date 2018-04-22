d9c57ec119e2a7133f3cc2ec7637fc85b9cc0dfe
adithwip <adithwidyapradipta@gmail.com>
2018-04-22 11:20:42 +0700

    First app with swift and XCode!!

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by HIJUP on 21/04/18.
+//  Copyright © 2018 Adith Widya Pradipta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
